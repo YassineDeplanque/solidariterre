4d42939f5f16eab454a8cbbd96b82348c82371a8
Yassine Deplanque <yassine59958@gmail.com>
2025-09-12 10:57:33 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
 function App() {
   return (
-<div>
-  <h1>
-    Hello world
-  </h1>
-</div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
